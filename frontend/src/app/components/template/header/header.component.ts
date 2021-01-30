ee081aef6a645baa3354bb98e8d4a61dd9015e87
fabsts <fabi.santos@edu.pbh.gov.br>
2021-01-30 14:37:11 -0300

    Header funionando

--- frontend/src/app/components/template/header/header.component.ts
+++ frontend/src/app/components/template/header/header.component.ts
@@ -2,13 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  template: `
-    <p>
-      header works!
-    </p>
-  `,
-  styles: [
-  ]
+
+  styleUrls: ['./header.component.css'],
+  templateUrl: './header.component.html',
+
+
 })
 export class HeaderComponent implements OnInit {
 
